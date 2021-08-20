8677e073c2953ac5973e543562fd07c0bf0ad59e
zengjia <zengjia1@bluemoon.com.cn>
2021-08-20 22:56:02 +0800

    数独 优化空间复杂度

--- src/main/java/pruning/SudokuSolver_37.java
+++ src/main/java/pruning/SudokuSolver_37.java
@@ -5,9 +5,9 @@ package pruning;
  * @date 2021-08-20 22:19:37
  */
 public class SudokuSolver_37 {
-    boolean[][] row = new boolean[9][10];
-    boolean[][] col = new boolean[9][10];
-    boolean[][] box = new boolean[9][10];
+    boolean[][] row = new boolean[9][9];
+    boolean[][] col = new boolean[9][9];
+    boolean[][] box = new boolean[9][9];
 
     public void solveSudoku(char[][] board) {
         for (int i = 0; i < board.length; i++) {
@@ -15,7 +15,7 @@ public class SudokuSolver_37 {
                 if (board[i][j] == '.') {
                     continue;
                 }
-                int curNum = board[i][j] - '0';
+                int curNum = board[i][j] - '1';
                 row[i][curNum] = true;
                 col[j][curNum] = true;
                 int whichBox = j / 3 + (i / 3) * 3;
@@ -30,7 +30,7 @@ public class SudokuSolver_37 {
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == '.') {
                     for (char c = '1'; c <= '9'; c++) {
-                        int curNum = c - '0';
+                        int curNum = c - '1';
                         int whichBox = j / 3 + (i / 3) * 3;
                         if (!row[i][curNum] && !col[j][curNum] && !box[whichBox][curNum]) {
                             row[i][curNum] = true;
--- src/main/java/pruning/ValidSudoku_36.java
+++ src/main/java/pruning/ValidSudoku_36.java
@@ -6,9 +6,9 @@ package pruning;
  */
 public class ValidSudoku_36 {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] row = new boolean[9][10];
-        boolean[][] col = new boolean[9][10];
-        boolean[][] box = new boolean[9][10];
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
@@ -16,7 +16,7 @@ public class ValidSudoku_36 {
                     continue;
                 }
 
-                int curNum = board[i][j] - '0';
+                int curNum = board[i][j] - '1';
 
                 if (row[i][curNum]) {
                     return false;
