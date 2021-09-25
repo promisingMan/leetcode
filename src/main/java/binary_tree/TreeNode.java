9037704a6015510f6253005f5f73c78c067d4f3f
zengjia <zengjia1@bluemoon.com.cn>
2021-09-25 18:01:20 +0800

    +

--- src/main/java/binary_tree/TreeNode.java
+++ src/main/java/binary_tree/TreeNode.java
@@ -2,13 +2,19 @@ package binary_tree;
 
 public class TreeNode {
     int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
